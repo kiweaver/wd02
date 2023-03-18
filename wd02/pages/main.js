49381c3d7753d132d0079aadb3de70bea0b35b7a
pkern <123541814+kiweaver@users.noreply.github.com>
2023-03-18 22:41:05 +1300

    css styling unordered list/ buttons

--- wd02/pages/main.js
+++ wd02/pages/main.js
@@ -1,24 +1,27 @@
+/*Add function*/
+//The text inbox is cleared for the next item.
+
 let ul = document.querySelector('ul');
 let input = document.querySelector('input');
 let btn = document.querySelector('#clicker');
 let remove = document.querySelector('#remove');
-
+//this function adds items to the list.
 btn.addEventListener('click', () => {
   let newLi = document.createElement("li")
   newLi.innerHTML = `${input.value}`
   ul.appendChild(newLi)
   input.value = ''
 })
-
+//This function removes items on the list.
 remove.addEventListener('click', () => {
  ul.removeChild(ul.lastElementChild)
   input.value = ''
 })
 
-
 let li = document.querySelectorAll('li');
-
+//This function strikes through an item on the list.
 ul.addEventListener('click', (event) => {
   if (!event.target.matches('li')) return
   event.target.classList.toggle('strike')
 })
+
